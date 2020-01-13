675ee8e034228391d6fa62af57966e7a444c11be
Jame <tranj52@mcmaster.ca>
2020-01-13 04:26:37 -0500

    minor refactoring

--- twitter.py
+++ twitter.py
@@ -1,39 +1,53 @@
 import tweepy
 from textblob import TextBlob
 from tweepy.streaming import StreamListener
+from tweepy import Stream
 
-
+#  Twitter login info
 consumer_key = 'SgGPLEywoVoJwJULMCoPof5vN'
 consumer_secret = 'uVEWE7OJojnqSCqAMqVW4JTdS9eYjwrqJHOVaoCdkx0galxnE9'
 
 access_token = '1041168468739932161-IHJP2EaK1NzW1fWdTbHZ2q3EDby7Od'
 access_token_secret = 's4DeJvLdsZ5QdHpNGS1F9BYAtTRs4eALh5gxzyYNGHAnm'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  # login
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
+class StdOutListener(StreamListener):  # Inherits from built in StreamListener
+    def __init__(self, tweet_filename):
+        self.tweet_filename = tweet_filename
 
-class StdOutListener(StreamListener):
-    def on_data(self, data):  # takes in info from streamlistener
-        print(data)
+    def on_data(self, data):  # writes to tweet_filename
+        try:
+            with open(self.tweet_filename, 'a') as fp:  
+                fp.write(data)
 
-    def on_error(self, status):
+        except BaseException as identifier:
+            print("Error on data; %s" % identifier)
+
+    def on_error(self, status):  # prints out the error message
         pass
 
 
 # TWITTER STREAMER ##
+# An object that processes live tweets
 class TwitterStreamer():
     def __init__(self):
         pass
 
-    def stream_tweets(self, a, b):
-        
+    def stream_tweets(self, tweet_filename, hash_tag_list):
+        listener = StdOutListener(tweet_filename)  # creates a listener object
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  # login
+        auth.set_access_token(access_token, access_token_secret)
+        # api = tweepy.API(auth)
+        stream = Stream(auth, listener)
+        stream.filter(track=hash_tag_list)
 
-# asdasd ## asdasd
 
-
-
+if __name__ == "__main__":
+    hash_tag_list = ["Donald Trump", "Bernie Sanders"]
+    tweet_filename = "tweets.json"
+    
+    streamer = TwitterStreamer()
+    streamer.stream_tweets(tweet_filename, hash_tag_list)
 
 
 # public_tweets = api.search('WW3')
